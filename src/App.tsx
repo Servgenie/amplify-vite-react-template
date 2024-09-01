d7e195a7778509bf841d14233d175a18c6030c40
Key Cayetano <inkeyygnito@keys-mbp.mynetworksettings.com>
2024-09-01 01:55:03 -0400

    Added Servgenie Logo and Texts

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
+import { Flex, Heading, Image, Text } from "@aws-amplify/ui-react";
 
 const client = generateClient<Schema>();
 
@@ -23,7 +24,33 @@ function App() {
 
   return (
     <main>
-      <h1>Servgenie Roadmap</h1>
+      <Flex
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        alignContent="flex-start"
+        wrap="nowrap"
+        gap="0.5rem"
+      >
+        <Image
+          alt="Servgenie Logo"
+          src="/servgenie-logo.png"
+          objectFit="initial"
+          objectPosition="50% 50%"
+          backgroundColor="initial"
+          onClick={() => alert("📸 Say cheese!")}
+        />
+        <Heading level={1}>SERVGENIE</Heading>
+        <Heading level={3}>Any wish, anytime, anywhere.</Heading>
+        <Text
+          variation="tertiary"
+          fontWeight={200}
+          fontSize="1.2em"
+          fontStyle="italic"
+        >
+          Coming soon...
+        </Text>
+      </Flex>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
@@ -32,13 +59,7 @@ function App() {
           </li>
         ))}
       </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/">
-          Learn about AWS Amplify here.
-        </a>
-      </div>
+      <div>🥳 In the mean time, leave us your wishes.</div>
     </main>
   );
 }
