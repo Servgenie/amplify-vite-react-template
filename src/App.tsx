4817acea3f7ea5c35b1d376ae2e6b8c8b3830044
Juvert Jay Fetalino <jayfetalino@gmail.com>
2024-09-14 22:07:18 -0400

    Jay/Feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,44 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
-import { Flex, Heading, Image, Text } from "@aws-amplify/ui-react";
+import {
+  Flex,
+  Heading,
+  Image,
+  Text,
+  SelectField,
+  Card,
+  View,
+  Badge,
+  Divider,
+  Button,
+} from "@aws-amplify/ui-react";
 
 const client = generateClient<Schema>();
 
+
+
+const items = [
+  {
+    title: "Coach Genie",
+    badges: ["Waterfront", "Verified"],
+  },
+  {
+    title: "Caregiver Genie",
+    badges: ["Mountain", "Verified"],
+  },
+  {
+    title: "Milford - Room #3",
+    badges: ["Mountain", "Verified"],
+  },
+];
+
+
+
+
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
+  let value = 0;
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
@@ -51,6 +82,79 @@ function App() {
           Coming soon...
         </Text>
       </Flex>
+
+      <SelectField
+        label="Select A Genie"
+        size="large"
+        placeholder="Please Select Your Wish"
+       
+      >
+        <option  value="0">Fitness Genie</option>
+        <option  value="1">Care Genie</option>
+        <option value="2">Cosmetologist Genie</option>
+      </SelectField>
+
+      <Card borderRadius="medium" maxWidth="20rem" variation="outlined">
+        <Image
+          src="/road-to-milford-new-zealand-800w.jpg"
+          alt="Demonstrate and explain how to perform various exercises and routines to minimize injuries and improve fitness"
+        />
+        <View padding="xs">
+          <Flex>
+            {items[0].badges.map((badge) => (
+              <Badge
+                key={badge}
+                backgroundColor={
+                  badge === "Waterfront"
+                    ? "blue.40"
+                    : badge === "Mountain"
+                    ? "green.40"
+                    : "yellow.40"
+                }
+              >
+                {badge}
+              </Badge>
+            ))}
+          </Flex>
+          <Divider padding="xs" />
+          <Heading padding="medium">{items[0].title}</Heading>
+          <Button variation="primary" isFullWidth>
+            Book it
+          </Button>
+        </View>
+      </Card>
+
+      <Card borderRadius="medium" maxWidth="20rem" variation="outlined">
+        <Image
+          src="/road-to-milford-new-zealand-800w.jpg"
+          alt="A professional who assists individuals with daily tasks and activities so that their patients can live more independent lives"
+        />
+        <View padding="xs">
+          <Flex>
+            {items[1].badges.map((badge) => (
+              <Badge
+                key={badge}
+                backgroundColor={
+                  badge === "Waterfront"
+                    ? "blue.40"
+                    : badge === "Mountain"
+                    ? "green.40"
+                    : "yellow.40"
+                }
+              >
+                {badge}
+              </Badge>
+            ))}
+          </Flex>
+          <Divider padding="xs" />
+          <Heading padding="medium">{items[1].title}</Heading>
+          <Button variation="primary" isFullWidth>
+            Book it
+          </Button>
+        </View>
+      </Card>
+
+
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
@@ -59,6 +163,7 @@ function App() {
           </li>
         ))}
       </ul>
+
       <div>In the mean time, leave us your wishes.</div>
     </main>
   );
